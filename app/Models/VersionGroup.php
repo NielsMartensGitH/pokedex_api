87186f5c8cf71ebf2418a2f8c150441463697cae
Niels <niels_martens@live.nl>
2022-05-05 21:43:26 +0200

    created migrations and models

---
+++ app/Models/VersionGroup.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VersionGroup extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_05_05_181648_create_move_version_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('move_version_groups', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('move_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('version_group_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('move_learn_method_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('move_version_groups');
+    }
+};
