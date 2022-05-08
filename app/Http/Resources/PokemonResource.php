a0e3dea21e50dae45e7a261380cae96ae82cb299
Niels <niels_martens@live.nl>
2022-05-08 12:50:08 +0200

    set PokemonResource withoutWrapping, now there is no 'data' key at beginning of json response

--- app/Http/Resources/PokemonResource.php
+++ app/Http/Resources/PokemonResource.php
@@ -4,8 +4,6 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\SpriteResource;
-use App\Http\Resources\TypeResource;
-use App\Models\Sprite;
 
 class PokemonResource extends JsonResource
 {
@@ -15,13 +13,13 @@ class PokemonResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
+
     public function toArray($request)
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
             'sprites' => SpriteResource::collection($this->sprites)->where('sprite_category_id', 5)->first(),
-            // 'types' =>  TypeResource::collection($this->types)
             'types' => PokemonTypeResource::collection($this->pokemon_types)
         ];
     }
